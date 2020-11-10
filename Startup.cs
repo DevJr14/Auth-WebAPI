bb1cc5ad7a9819198a825713f231464dfaf1601f
Junior Matlou <junianohmm6@gmail.com>
2020-11-10 11:41:59 +0200

    Adds JWT  Bearer Package to the project.

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -36,6 +37,13 @@ namespace WebAPI
                 .AddEntityFrameworkStores<AuthDbContext>();
 
             services.AddCors();
+
+            //JWT Authentication
+
+            services.AddAuthentication(auth  =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
